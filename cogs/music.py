49f99924e2b54bee58f0d52685caee4b97a0e5a7
devair restani <devair.restani@educacross.com.br>
2022-01-17 15:30:29 -0300

    updates

--- cogs/music.py
+++ cogs/music.py
@@ -1,9 +1,12 @@
+from typing import BinaryIO
 import discord
 from discord.channel import VoiceChannel
 from discord.ext import commands
 from discord.voice_client import VoiceProtocol
 
 from pytube import YouTube, Playlist, Search
+
+
 
 class music(commands.Cog):
     def __init__(self, client):
@@ -18,26 +21,33 @@ class music(commands.Cog):
 
         self.vc: VoiceProtocol = None 
         self.volume = 1.0
-        self.playing_now = ""
+        self.playing_now: YouTube = None
+        
+        self.colors = {
+            "red": 0xFF3333,
+            "green": 0x33FF33,
+            "blue": 0x3333FF,
+            "yellow": 0xFFFF33,
+            "purple": 0xFF33FF,
+            "cyan": 0x33FFFF,
+            "white": 0xFFFFFF,
+            "black": 0x000000
+        }
 
     #searching the item on youtube
     def search_yt(self, item):
-        res = []
         try:
-            if "playlist" in item:
+            if "/playlist" in item:
                 pl = Playlist(item)
-                for video in pl.videos:
-                    res.append(video)
+                return pl.videos
             elif "https://youtu.be" in item or "https://www.youtube.com" in item or "https://music.youtube.com" in item:
-                res.append(YouTube(item))
+                return [YouTube(item)]
             else:
-                res.append(Search(item).results[0])
+                return [Search(item).results[0]]
 
         except:
             return None
 
-        
-        return res
 
 
     def play_next(self):
@@ -45,27 +55,27 @@ class music(commands.Cog):
             self.is_playing = True
 
             #get the first url
-            last_song = self.music_queue.pop()
+            last_song = self.music_queue.pop(0)
             song: YouTube = last_song[0]
-            s_url = song.streams.filter(only_audio=True).first().url
+            s_url = song.streams.get_audio_only().url
 
             #remove the first element as you are currently playing it
-            self.playing_now = song.title
+            self.playing_now = song
 
-            self.vc.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(s_url, executable='ffmpeg.exe', **self.FFMPEG_OPTIONS), volume=self.volume), after=lambda e: self.play_next())
+            self.vc.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(s_url, **self.FFMPEG_OPTIONS), volume=self.volume), after=lambda e: self.play_next())
         else:
             self.is_playing = False
 
     # infinite loop checking 
     async def play_music(self):
         if len(self.music_queue) > 0:
-            self.is_playing = True
 
-            last_song = self.music_queue.pop()
+            last_song = self.music_queue.pop(0)
+                        
             song: YouTube = last_song[0]
             voiceChannel: VoiceChannel = last_song[1]
 
-            s_url = song.streams.filter(only_audio=True).first().url
+            s_url = song.streams.get_audio_only().url
             
             #try to connect to voice channel if you are not already connected
 
@@ -74,13 +84,13 @@ class music(commands.Cog):
             else:
                 await self.vc.move_to(voiceChannel)
             
-            #remove the first element as you are currently playing it
-            self.playing_now = song.title
+            self.playing_now = song
             
-            if len(self.music_queue) > 0:
+            if self.is_playing:
                 self.play_next()
             else:
-                self.vc.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(s_url, executable='ffmpeg.exe', **self.FFMPEG_OPTIONS), volume=self.volume), after=lambda e: self.play_next())
+                self.is_playing = True
+                self.vc.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(s_url, **self.FFMPEG_OPTIONS), volume=self.volume), after=lambda e: self.play_next())
         else:
             self.is_playing = False
             await self.vc.disconnect()
@@ -96,7 +106,7 @@ class music(commands.Cog):
         #if voice_channel is None:
             #you need to be connected so that the bot knows where to go
             embedvc = discord.Embed(
-                colour= 1646116,#grey
+                colour= self.colors["cyan"],#grey
                 description = 'Para tocar uma música, primeiro se conecte a um canal de voz.'
             )
             await ctx.send(embed=embedvc)
@@ -105,20 +115,20 @@ class music(commands.Cog):
             songs = self.search_yt(query)
             if type(songs) == type(True):
                 embedvc = discord.Embed(
-                    colour= 12255232,#red
+                    colour= self.colors["red"],#red
                     description = 'Algo deu errado! Tente mudar ou configurar a playlist/vídeo ou escrever o nome dele novamente!'
                 )
                 await ctx.send(embed=embedvc)
             else:
                 if songs == None:
                     embedvc = discord.Embed(
-                        colour= 12255232,#red
+                        colour= self.colors["red"],#red
                         description = 'Ocorreu um erro!'
                     )
                     await ctx.send(embed=embedvc)
                 if len(songs) == 0:
                     embedvc = discord.Embed(
-                        colour= 12255232,#red
+                        colour= self.colors["red"],#red
                         description = 'Não encontrei nenhum resultado para a pesquisa!'
                     )
                     await ctx.send(embed=embedvc)
@@ -141,25 +151,37 @@ class music(commands.Cog):
                     await self.play_music()
 
     @commands.command(name="queue", help="Mostra as atuais músicas da fila.",aliases=['q','fila'])
-    async def q(self, ctx):
+    async def q(self, ctx, *args):
         retval = ""
+        
+        page = 0
+        items_per_page = 10
+        
+        try:
+            page = int(args[0])
+            page -= 1
+        except:
+            embedvc = discord.Embed(
+                colour= self.colors["cyan"],
+                description = 'Insira um numero de pagina'
+            )
 
         if len(self.music_queue) == 0:
             embedvc = discord.Embed(
-                colour= 1646116,
-                description = 'Não existe músicas na fila no momento.'
+                colour= self.colors["red"],
+                description = 'Não existem músicas na fila no momento.'
             )
             await ctx.send(embed=embedvc)
 
-        for i in range(0, len(self.music_queue)):
+        for i in range(page * items_per_page, page * items_per_page + items_per_page):
             retval += f'**{i+1} - **' + self.music_queue[i][0].title + "\n"
-            if len(retval) > 4000:
-                embedvc = discord.Embed(
-                    colour= 12255232,
-                    description = f"{retval}"
-                )
-                await ctx.send(embed=embedvc)
-                retval = ""
+        
+        embedvc = discord.Embed(
+            colour= self.colors["green"],
+            description = retval
+        )
+        await ctx.send(embed=embedvc)
+           
             
 
     @commands.command(name="skip", help="Pula a atual música que está tocando.",aliases=['pular', 'sk'])
@@ -169,7 +191,7 @@ class music(commands.Cog):
             #try to play next in the queue if it exists
             await self.play_music()
             embedvc = discord.Embed(
-                colour= 1646116,#ggrey
+                colour= self.colors,#ggrey
                 description = f"Você pulou a música."
             )
             await ctx.send(embed=embedvc)
@@ -179,7 +201,7 @@ class music(commands.Cog):
         if self.vc != "" and self.vc:
             self.vc.stop()
             embedvc = discord.Embed(
-                colour= 1646116,#grey
+                colour= self.colors["cyan"],#grey
                 description = f"Você parou a música."
             )
             await ctx.send(embed=embedvc)
@@ -189,7 +211,7 @@ class music(commands.Cog):
         if self.vc != "" and self.vc:
             self.vc.pause()
             embedvc = discord.Embed(
-                colour= 1646116,#grey
+                colour= self.colors["cyan"],#grey
                 description = f"Você pausou a música."
             )
             await ctx.send(embed=embedvc)
@@ -199,7 +221,7 @@ class music(commands.Cog):
         if self.vc != "" and self.vc:
             self.vc.resume()
             embedvc = discord.Embed(
-                colour= 1646116,#grey
+                colour= self.colors["cyan"],#grey
                 description = f"Você resumiu a música."
             )
             await ctx.send(embed=embedvc)
@@ -210,7 +232,7 @@ class music(commands.Cog):
             self.vc.source.volume = volume/100
             self.volume = volume/100
             embedvc = discord.Embed(
-                colour= 1646116,#grey
+                colour= self.colors["cyan"],#grey
                 description = f"Você ajustou o volume para **{volume}%**."
             )
             await ctx.send(embed=embedvc)
@@ -219,22 +241,30 @@ class music(commands.Cog):
     async def nowplaying(self, ctx):
         if self.vc != "" and self.vc:
             embedvc = discord.Embed(
-                colour= 1646116,#grey
-                description = f"Está tocando: **{self.playing_now}**"
+                colour= self.colors["cyan"],#grey
+                description = f"Está tocando: **{self.playing_now.title}**"
             )
             await ctx.send(embed=embedvc)
         else:
             embedvc = discord.Embed(
-                colour= 1646116,#grey
+                colour= self.colors["yellow"],#grey
                 description = 'Não há nada tocando no momento.'
             )
             await ctx.send(embed=embedvc)
 
     @commands.command(name="ping", help="Mostra o ping do bot.")
     async def ping(self, ctx):
+        ping = round(self.client.latency*1000)
+        color = self.colors["green"]
+        
+        if ping > 200:
+            color = self.colors["red"]
+        elif ping > 100:
+            color = self.colors["yellow"]
+        
         embedvc = discord.Embed(
-            colour= 1646116,#grey
-            description = f"O ping do bot é **{round(self.client.latency*1000)}ms**"
+            colour= color,
+            description = f"O ping do bot é **{ping}ms**"
         )
         await ctx.send(embed=embedvc)
     
@@ -242,7 +272,7 @@ class music(commands.Cog):
     async def clear(self, ctx):
         self.music_queue = []
         embedvc = discord.Embed(
-            colour= 1646116,#grey
+            colour= self.colors["cyan"],#grey
             description = f"A fila foi limpa."
         )
         await ctx.send(embed=embedvc)    
@@ -251,7 +281,7 @@ class music(commands.Cog):
     async def skip_error(self,ctx,error):
         if isinstance(error, commands.MissingPermissions):
             embedvc = discord.Embed(
-                colour= 12255232,
+                colour= self.colors["red"],
                 description = f"Você precisa da permissão **Gerenciar canais** para pular músicas."
             )
             await ctx.send(embed=embedvc)     
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import discord
 from discord.ext import commands
 import os
+import time
 
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -15,5 +16,8 @@ client = commands.Bot(command_prefix = "-", case_insensitive = True)
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+  
 
 client.run(TOKEN)
+
+print("Bot ok...")
